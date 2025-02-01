4050757b2ee2cbf2bbd2044d6c67c8339d99a108
ArdilagoSurbakti <ardilago4@gmail.com>
2025-02-02 03:40:56 +0700

    1.1

--- app/view-lapor/route.js
+++ app/view-lapor/route.js
@@ -1,20 +1,26 @@
-// app/api/pengaduan/route.js
-import { PrismaClient } from '@prisma/client';
-import { NextResponse } from 'next/server';
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    // Mengambil semua data pengaduan dari database
     const pengaduan = await prisma.feedback.findMany();
+console.log(pengaduan);
 
-    // Mengirim data pengaduan ke client
-    return NextResponse.json({
+    // Buat response dengan header CORS
+    const response = NextResponse.json({
       status: 200,
       message: "Data pengaduan berhasil diambil.",
       data: pengaduan,
     });
+
+    // Tambahkan header CORS
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+    return response;
   } catch (error) {
     return NextResponse.json({
       status: 500,
